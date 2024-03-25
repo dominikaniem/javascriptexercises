cd2ba80f49e0ff02e0e775eedc462e018bd26b3d
Dominika N <dominikaniem@gmail.com>
2024-03-25 22:05:49 +0100

    operators & objects

--- objects.js
+++ objects.js
@@ -1,16 +1,24 @@
 // arytmetyczne
 
-let nbr = 5
-console.log(nbr + 5);
-console.log(nbr - 5);
-console.log(nbr * 5);
-console.log(nbr / 5);
-console.log(nbr % 5);
-console.log(nbr ** 5);
+// let nbr = 5
+// console.log(nbr + 5);
+// console.log(nbr - 5);
+// console.log(nbr * 5);
+// console.log(nbr / 5);
+// console.log(nbr % 5);
+// console.log(nbr ** 5);
 
 // przypisania
 
 nbr = 10;
-nbr +- 1; // nbr = nbr + 1
-nbr *= 2
+nbr += 1; // nbr = nbr + 1
+nbr *= 2;
+nbr ++;
+nbr --;
+
 console.log(nbr)
+
+
+// porównania
+
+//logiczne 
