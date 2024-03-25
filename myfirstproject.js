37eb7d2b3bf8aae846260c3909687a5b4148e9a4
Dominika N <dominikaniem@gmail.com>
2024-03-25 21:34:56 +0100

    Starting_over

--- myfirstproject.js
+++ myfirstproject.js
@@ -1,6 +1,6 @@
 var date = Date()
-console.log("hello word, today is "+ date); //date
-
+console.log("Hello word! Today is "+ date); //date
+/*Will type on console hello world and current time*/
 
 
 {
@@ -8,5 +8,5 @@ console.log("hello word, today is "+ date); //date
     console.log(a)
 }
 
-let myAge = 35
+let myAge = 36
 console.log("This is my age in 5 years: " + (myAge+5));
