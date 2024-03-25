ee7e4c438500e52b4ba0e388a4ba3f5af2e071f8
Dominika N <dominikaniem@gmail.com>
2024-03-25 21:53:11 +0100

    objects

--- objects.js
+++ objects.js
@@ -1,1 +1,16 @@
-// nowy kod 
+// arytmetyczne
+
+let nbr = 5
+console.log(nbr + 5);
+console.log(nbr - 5);
+console.log(nbr * 5);
+console.log(nbr / 5);
+console.log(nbr % 5);
+console.log(nbr ** 5);
+
+// przypisania
+
+nbr = 10;
+nbr +- 1; // nbr = nbr + 1
+nbr *= 2
+console.log(nbr)
